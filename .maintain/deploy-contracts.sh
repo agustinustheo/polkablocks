25f7a45e378a0dc32d1144cf7a01c01b4351b647
Theo <agustinustheoo@gmail.com>
2025-09-09 14:01:54 +0800

    chore: reorganize deployment scripts and docker files into .maintain directory

--- .maintain/deploy-contracts.sh
+++ .maintain/deploy-contracts.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Deploy Subscription Contract to Polkadot Asset Hub Testnet
+# This script should be run from the .maintain directory
 
 # Load environment variables
 source .env
+
+# Navigate to contracts directory
+cd ../contracts
 
 # Deploy the contract
 forge script script/Deploy.s.sol:DeploySubscription \
